d84f031cb0bc1e33093f5257286afccbf67db344
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-09 08:30:06 +0100

    Ejercicios header

---
+++ 01Ejercicios/EjerciciosBasicos/header.php
@@ -0,0 +1,9 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Refresh: 10; $_SERVER[PHP_SELF]");
+echo "<h1>Fecha actual: " . date("d/m/Y H:i:s") . "</h1>";
+echo "<br>";
+echo "<b>Con la funcion header() hemos especfificado que esta pagina no se guarde en la memoria cache, sino
+ que se llame a si mismadesde la pagina original cada 10 segundos. Puede comprobarse dejando la pagina 
+ sin actualizar durante 10 segundos o pulsando sobre actualizar</b>";
+
