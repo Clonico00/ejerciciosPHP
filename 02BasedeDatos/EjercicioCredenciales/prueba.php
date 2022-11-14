3ab81127d550cbbd5c7d2210bfa45f68fe792daa
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-14 13:37:56 +0100

    updates

--- 02BasedeDatos/EjercicioCredenciales/prueba.php
+++ 02BasedeDatos/EjercicioCredenciales/prueba.php
@@ -1,5 +1,32 @@
-<?php
-require_once 'lib/conexion.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio Tienda</title>
+</head>
+<body>
+<h3>Ejercicio: Login BBDD</h3>
+<hr>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <p>
+        <label>Usuario: </label>
+        <input type="text" name="usuario" required>
+        <label>Contraseña: </label>
+        <input type="password" name="password" required>
+        <input type="submit" name="enviar" value="Login">
+    </p>
+</form>
+<hr>
+</body>
+</html>
 
-$conexion = new Conexion();
-$conexion->conectar();
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once 'lib/conexion.php';
+
+    $conexion = new Conexion();
+    $conexion->conectar();
+    
+    
+}
+?>
