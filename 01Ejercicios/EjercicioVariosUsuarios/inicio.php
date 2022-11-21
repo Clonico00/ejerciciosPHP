ba3df529f31039d60185ce6aa8820936df319051
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-21 13:14:52 +0100

    ejercicio varios usuarios

--- 01Ejercicios/EjercicioVariosUsuarios/inicio.php
+++ 01Ejercicios/EjercicioVariosUsuarios/inicio.php
@@ -1,4 +1,3 @@
 <?php
 echo "<h2>Bienvenido</h2>";
-/*Volver a la pagina de login*/
 echo "<a href='login.php'>Volver a la pagina de login</a>";
--- 01Ejercicios/EjercicioVariosUsuarios/login.php
+++ 01Ejercicios/EjercicioVariosUsuarios/login.php
@@ -1,100 +1,46 @@
 <?php
-/*Comprobar si el usuario le ha dado a recordar en el checkbox, de ser asi guardar los datos en una cookie y en los input "usuario" y "password", escribir el usuario y la contraseña pero con "*"*/
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_COOKIE["usuario"] && isset($_POST['cbox1'])) {
-        setcookie("usuario", $_POST['usuario'], time() + 60 * 60 * 24 * 365);
-        setcookie("password", $_POST['password'], time() + 60 * 60 * 24 * 365);
-        $usuario = str_replace($_COOKIE["usuario"], str_repeat("*", strlen($_COOKIE["usuario"])), $_COOKIE["usuario"]);
-        $password = str_replace($_COOKIE["password"], str_repeat("*", strlen($_COOKIE["password"])), $_COOKIE["password"]);
-        ?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Ejercicio Varios Usuarios</title>
-        </head>
-        <body>
-        <h2>Iniciar Sesion</h2>
-        <hr>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <label for="usuario">Usuario: </label>
-            <?php echo "<input type='text' name='usuario' value='" . $usuario . "'><br>"; ?>
-            <br>
-            <br>
-            <label for="password">Contraseña: </label>
-            <?php echo "<input type='text' name='password' value='" . $password . "'><br>"; ?>
-            <br>
-            <br>
-            <label><input type="radio" name="cbox1"> No cerrar sesion </label><br>
-            <br>
-            <input type="submit" value="Iniciar sesion" name="iniciarSesion">
-        </form>
-        <hr>
-        <a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">Iniciar sesion con otra cuenta</a>
-        </body>
-        </html>
-        <?php
-    } else {
-        ?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Ejercicio Varios Usuarios</title>
-        </head>
-        <body>
-        <h2>Iniciar Sesion</h2>
-        <hr>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <label for="usuario">Usuario: </label>
-            <input type="text" name="usuario" id="usuario" required>
-            <br>
-            <br>
-            <label for="password">Contraseña: </label>
-            <input type="text" name="password" id="password" required>
-            <br>
-            <br>
-            <label><input type="radio" name="cbox1"> No cerrar sesion </label><br>
-            <br>
-            <input type="submit" value="Iniciar sesion" name="iniciarSesion">
-        </form>
-        <hr>
-        <a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">Iniciar sesion con otra cuenta</a>
-        </body>
-        </html>
-        <?php
+session_start();
+if (isset($_POST['enviar'])) {
+    if (isset($_POST['usuario']) && isset($_POST['password'])) {
+        if ($_POST['usuario'] == 'admin' && $_POST['password'] == 'admin') {
+            $_SESSION['usuario'] = $_POST['usuario'];
+            $_SESSION['password'] = $_POST['password'];
+            if (isset($_POST['recordar'])) {
+                setcookie('usuario', $_POST['usuario'], time() + 60 * 60 * 24 * 365);
+                setcookie('password', $_POST['password'], time() + 60 * 60 * 24 * 365);
+            }
+            header('Location: inicio.php');
+        } else {
+            echo "<h2>Usuario o contraseña incorrectos</h2>";
+        }
     }
-} else {
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Ejercicio Varios Usuarios</title>
-    </head>
-    <body>
-    <h2>Iniciar Sesion</h2>
-    <hr>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <label for="usuario">Usuario: </label>
-        <input type="text" name="usuario" id="usuario" required>
-        <br>
-        <br>
-        <label for="password">Contraseña: </label>
-        <input type="text" name="password" id="password" required>
-        <br>
-        <br>
-        <label><input type="radio" name="cbox1"> No cerrar sesion </label><br>
-        <br>
-        <input type="submit" value="Iniciar sesion" name="iniciarSesion">
-    </form>
-    <hr>
-    <a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">Iniciar sesion con otra cuenta</a>
-    </body>
-    </html>
-    <?php
 }
 ?>
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario" id="usuario" value="<?php if (isset($_COOKIE['usuario'])) {
+            echo $_COOKIE['usuario'];
+        } ?>">
+        <br>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" value="<?php if (isset($_COOKIE['password'])) {
+            echo $_COOKIE['password'];
+        } ?>">
+        <br>
+        <input type="checkbox" name="recordar" id="recordar" <?php if (isset($_COOKIE['usuario'])) {
+            echo 'checked';
+        } ?>>
+        <label for="recordar">Recordar usuario</label>
+        <br>
+        <input type="submit" name="enviar" value="Enviar">
+    </form>
+</body>
+</html>
 
