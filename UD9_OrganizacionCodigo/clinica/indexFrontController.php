8456ac9547837ce3a779064893ff5c906f744c76
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-23 11:33:42 +0100

    updates mvc clinica

--- UD9_OrganizacionCodigo/clinica/indexFrontController.php
+++ UD9_OrganizacionCodigo/clinica/indexFrontController.php
@@ -11,16 +11,6 @@ frontController::main();
 
 ?>
 
-<nav>
-    <ul>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php">Inicio</a></li>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Paciente&action=mostrarTodos">Mostrar todos mis pacientes</a></li>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Paciente&action=buscarNombre">Buscar pacientes por su nombre</a></li>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Paciente&action=buscarNombre">Buscar pacientes por su nombre</a></li>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Doctor&action=listar">Listado de doctores</a></li>
-        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Doctor&action=registro">Registrar un nuevo doctor</a></li>
-    </ul>
-</nav>
 
 <?php
 require_once 'views/layout/footer.php';
---
+++ UD9_OrganizacionCodigo/clinica/views/layout/footer.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php">Inicio</a></li>
+        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Paciente&action=mostrarTodos">Mostrar todos mis pacientes</a></li>
+        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Paciente&action=buscarNombre">Buscar pacientes por su nombre</a></li>
+        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Doctor&action=listar">Listado de doctores</a></li>
+        <li><a href="http://localhost:63342/ejercicios/UD9_OrganizacionCodigo/clinica/indexFrontController.php?controller=Doctor&action=registro">Registrar un nuevo doctor</a></li>
+    </ul>
+</nav>
