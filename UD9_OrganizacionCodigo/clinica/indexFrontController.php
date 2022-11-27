d8bbbc0a5631bc89818929655ec648bd6ab5d247
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-27 16:51:14 +0100

    updates mvc clinica

--- UD9_OrganizacionCodigo/clinica/indexFrontController.php
+++ UD9_OrganizacionCodigo/clinica/indexFrontController.php
@@ -4,7 +4,7 @@ require_once 'config/config.php';
 require_once 'views/layout/header.php';
 require_once 'controllers/FrontController.php';
 
-use Controllers\FrontController;
+use \Controllers\FrontController;
 
 frontController::main();
 
